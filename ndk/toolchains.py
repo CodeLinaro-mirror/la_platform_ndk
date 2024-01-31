1e33a56b4ad8f5c218f8f9db1377bbffef948180
Dan Albert <danalbert@google.com>
2024-01-31 14:15:46 -0800

    Update LLVM to e build for r26c.
    Changelog update will land in main.
    
    Bug: https://github.com/android/ndk/issues/1928
    Bug: https://github.com/android/ndk/issues/1953
    Test: ./checkbuild.py && ./run_tests.py
    Change-Id: I0b9a2f9f85e3271da3823affa8e3ea9a225ccd4a

--- ndk/toolchains.py
+++ ndk/toolchains.py
@@ -21,7 +21,7 @@ from typing import List
 import ndk.paths
 from ndk.hosts import Host, get_default_host
 
-CLANG_VERSION = "clang-r487747d"
+CLANG_VERSION = "clang-r487747e"
 
 
 HOST_TRIPLE_MAP = {
