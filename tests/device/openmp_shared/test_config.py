860d9d5c4e254c7bd15d9be15a1301b04e451e67
Dan Albert <danalbert@google.com>
2024-09-18 13:59:08 -0700

    Remove XFAILs for openmp.
    Fixed with the latest toolchain (already merged).
    
    Bug: https://github.com/android/ndk/issues/2041
    Test: ./run_tests.py
    Change-Id: Iac0f99b30a735a925606ebff5a1a09cbb60557c1

--- tests/device/openmp_shared/test_config.py
+++
@@ -1,9 +0,0 @@
-from ndk.abis import Abi
-from ndk.test.devices import Device
-from ndk.test.devicetest.case import TestCase
-
-
-def run_broken(test: TestCase, _device: Device) -> tuple[str | None, str | None]:
-    if test.config.abi == Abi("x86"):
-        return "x86", "https://github.com/android/ndk/issues/2041"
-    return None, None
