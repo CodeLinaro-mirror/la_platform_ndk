a6f2da6f997cc07ac3024ae1471cfe556ec4cfbc
Dan Albert <danalbert@google.com>
2025-06-02 13:18:11 -0700

    Use the buildbot's Python for test builds.
    I'm running out of ideas. The Studio buildbots we use in CI have Python
    3.12 installed here. Try using that rather than the 3.11 we have in
    prebuilts.
    
    Change-Id: I14f7e4f5299a6fe062645800c1fa292efe7a2762

--- ci.ps1
+++ ci.ps1
@@ -1,3 +1,3 @@
 $EntryPoint = Join-Path -Path $PSScriptRoot -ChildPath ci.py
-$PythonPath = Join-Path -Path $PSScriptRoot -ChildPath ..\prebuilts\python\windows-x86\python.exe
+$PythonPath = C:\python312\python.exe
 & $PythonPath $EntryPoint $args
