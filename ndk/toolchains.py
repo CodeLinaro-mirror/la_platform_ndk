9650f3f573b43ebb31f38e8f4b5fe2f73fa825d2
Dan Albert <danalbert@google.com>
2025-08-28 13:53:35 -0700

    Update LLVM to r563880c.
    Change-Id: I4ae0788356eb4d67c7a34420ba3edc60c8b416c1

--- ndk/toolchains.py
+++ ndk/toolchains.py
@@ -21,7 +21,7 @@ from typing import List
 import ndk.paths
 from ndk.hosts import Host, get_default_host
 
-CLANG_VERSION = "clang-r563880b"
+CLANG_VERSION = "clang-r563880c"
 
 
 HOST_TRIPLE_MAP = {
