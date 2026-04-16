30739ff63cd787e0d592438f2dd14d24d6acf466
Pirama Arumuga Nainar <pirama@google.com>
2026-04-16 09:57:45 -0700

    Disable testing on static executables on API 21 devices
    Bug: 502794163
    
    Newer libc.a use MADV_WIPEONFORK and is expected to fail on devices
    with older kernels. Disable based on API level since the test scripts
    don't have a way to select based on the kernel version.
    
    Test: ./run_tests.py
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:18216a1e0f3a4b352a1a672c66947f90607bd381
    Merged-In: I69adf819cceeaa9715b98d0e49b28551e3bb525c
    Change-Id: I69adf819cceeaa9715b98d0e49b28551e3bb525c

--- tests/device/static_exe_lto/test_config.py
+++ tests/device/static_exe_lto/test_config.py
@@ -1,20 +1,7 @@
 from typing import Optional
 
-import ndk.abis
 from ndk.devenv.devices import Device
 from ndk.devenv.testrunner.case import TestCase
-from ndk.test.buildtest.case import Test
-
-
-def extra_cmake_flags() -> list[str]:
-    # Required for static executables.
-    return ["-DANDROID_PLATFORM=latest"]
-
-
-def override_runtime_minsdkversion(test: Test) -> int | None:
-    # We build as latest because static executables require that, but static executables
-    # are compatible with old OS versions.
-    return ndk.abis.min_api_for_abi(test.config.abi)
 
 
 def run_broken(test: TestCase, device: Device) -> tuple[Optional[str], Optional[str]]:
